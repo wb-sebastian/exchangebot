4f99f9135c800a138e2db22b7b54a3f39aadc408
wb-sebastian <sebastian.l.l.sebe@refkol.ro>
2025-07-26 00:44:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is awake!');
+});
+
+app.listen(port, () => {
+  console.log(`Web server running on port ${port}`);
+});
 require('dotenv').config();
 const { Client, GatewayIntentBits, PermissionsBitField } = require('discord.js');
 const axios = require('axios');
